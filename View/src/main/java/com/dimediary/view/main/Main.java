48f1ef07aa618eb67bfe451a3c8e62a4d7483a82
Marcus Kraft <marcus_kraft@posteo.de>
2016-07-07 20:49:38 +0200

    add project for view and link qt jambi

---
+++ View/src/main/java/com/dimediary/view/main/Main.java
@@ -0,0 +1,18 @@
+package com.dimediary.view.main;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		System.out.println("test");
+		
+		try {
+			Thread.sleep(20000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+}
